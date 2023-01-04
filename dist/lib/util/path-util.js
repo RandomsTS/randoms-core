c35dfcc7fb124f80ae0a0b4e16c499c6f6efc0d3
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2023-01-05 00:08:15 +0500

    caller path fix

--- dist/lib/util/path-util.js
+++ dist/lib/util/path-util.js
@@ -1,32 +1,15 @@
 "use strict";
-// export default function callsites() {
-// 	const _prepareStackTrace = Error.prepareStackTrace;
-// 	Error.prepareStackTrace = (_, stack) => stack;
-// 	const stack = new Error().stack?.slice(1); // eslint-disable-line unicorn/error-message
-// 	Error.prepareStackTrace = _prepareStackTrace;
-// 	return stack;
-// }
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getCallerPath = void 0;
-// /*@ts-ignore*/
-// console.log (callsites ());
-// /*@ts-ignore*/
-// console.log (callsites()[0].getFileName());
-/*returns caller File Path*/
+/* return caller file path */
 function getCallerFilePath() {
     try {
-        const error = new Error();
-        if (!error.stack)
-            throw new Error('No stack trace available.');
-        const stackLines = error.stack.split('\n');
-        const callerIndex = 2; // index of the caller in the stack trace
-        const callerLine = stackLines[callerIndex]; // get the line that contains the caller
-        const filePath = callerLine.match(/\((.+):\d+:\d+\)/); // extract the file path from the line
-        if (!filePath)
-            throw new Error('Could not find file path in stack trace.');
-        return filePath[1];
+        const err = new Error();
+        const regEx = new RegExp(/at Object.<anonymous> (\(.*?\))/);
+        return err.stack?.match(regEx)?.[0].match(/\(.*?\)/)?.[0]
+            .replace(/\(|\)/g, "").split(":", 2).join(":");
     }
-    catch (error) {
+    catch (err) {
         return undefined;
     }
 }
--- lib/util/path-util.ts
+++ lib/util/path-util.ts
@@ -1,30 +1,13 @@
-// export default function callsites() {
-// 	const _prepareStackTrace = Error.prepareStackTrace;
-// 	Error.prepareStackTrace = (_, stack) => stack;
-// 	const stack = new Error().stack?.slice(1); // eslint-disable-line unicorn/error-message
-// 	Error.prepareStackTrace = _prepareStackTrace;
-// 	return stack;
-// }
 
-// /*@ts-ignore*/
-// console.log (callsites ());
-// /*@ts-ignore*/
-// console.log (callsites()[0].getFileName());
-
-/*returns caller File Path*/ 
-function getCallerFilePath(): string  | undefined {
+/* return caller file path */
+function getCallerFilePath (): string | undefined
+{
     try {
-      const error =  new Error();
-      if (!error.stack) throw new Error('No stack trace available.');
-      const stackLines = error.stack.split('\n');
-      const callerIndex = 2; // index of the caller in the stack trace
-      const callerLine = stackLines[callerIndex]; // get the line that contains the caller
-      const filePath = callerLine.match(/\((.+):\d+:\d+\)/); // extract the file path from the line
-      if (!filePath) throw new Error('Could not find file path in stack trace.');
-      return filePath [1];
-    } catch (error) {
-      return undefined;
-    }
+        const err = new Error ();
+        const regEx =  new RegExp(/at Object.<anonymous> (\(.*?\))/);
+        return err.stack?.match(regEx)?.[0].match (/\(.*?\)/)?.[0]
+            .replace (/\(|\)/g, "").split (":", 2).join (":");
+    } catch (err) { return undefined;}
 }
 
 /* return current path with out extension */

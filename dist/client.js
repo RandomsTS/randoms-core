5cb1791e995041ac04b40e8a48b73ddddbcc38a4
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2023-01-05 21:31:34 +0500

    APIS improvements build

--- dist/client.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const execSync = require('child_process').execSync;
-const output = execSync('dir', { encoding: 'utf-8' });  // the default is 'buffer'
-console.log('Output was:\n', output);
--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,11 @@
 "use strict";
 /* internal */
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.init = exports.initRoute = exports.runServer = exports.useMiddlewares = void 0;
+exports.runServer = exports.useMiddlewares = void 0;
 // export { isValidType } from './lib/internals/typechecker';
 /* Public APIS */
 const RandomsRoute_1 = require("./lib/RandomsRoute");
 var server_1 = require("./lib/internals/server");
 Object.defineProperty(exports, "useMiddlewares", { enumerable: true, get: function () { return server_1.useMiddlewares; } });
 Object.defineProperty(exports, "runServer", { enumerable: true, get: function () { return server_1.runServer; } });
-var decorators_1 = require("./lib/decorators");
-Object.defineProperty(exports, "initRoute", { enumerable: true, get: function () { return decorators_1.initRoute; } });
-Object.defineProperty(exports, "init", { enumerable: true, get: function () { return decorators_1.init; } });
 exports.default = RandomsRoute_1.Route;

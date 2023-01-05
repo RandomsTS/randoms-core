2a7d31ea96e955607e58426f9ea3c21dccc7327e
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2023-01-05 20:41:47 +0500

    apis change

--- dist/lib/RandomsRoute.js
+++ dist/lib/RandomsRoute.js
@@ -5,15 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Route = void 0;
 const RouteBase_1 = __importDefault(require("./types/RouteBase"));
-function foo() {
-    console.log("foo");
-}
+const decorators_1 = require("./decorators");
 /* base class for all routes */
 class Route extends RouteBase_1.default {
     async get(req, res) { res.send(`can't resolve ${this.path}`); }
     async put(req, res) { res.send(`can't resolve ${this.path}`); }
     async post(req, res) { res.send(`can't resolve ${this.path}`); }
     async delete(req, res) { res.send(`can't resolve ${this.path}`); }
-    static fooFunc = foo;
+    static fooFunc = decorators_1.foo;
 }
 exports.Route = Route;
